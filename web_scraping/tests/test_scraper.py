45ffe68e4d2dfe3376543891d6c7797cc39f6116
guilhermearaujom <guilherme.matos@ccc.ufcg.edu.br>
2025-04-03 09:42:59 -0300

    test(scraper): atualizar testes para usar métodos genéricos com extensão de arquivo

--- web_scraping/tests/test_scraper.py
+++ web_scraping/tests/test_scraper.py
@@ -15,22 +15,21 @@ class TestScraper(unittest.TestCase):
         """
         self.scraper = Scraper(self.html)
     
-    def test_extract_all_pdf_links(self):
+    def test_extract_all_links_by_extension(self):
         expected_links = [
             "document_anexo_i.pdf",
             "document_anexo_ii.pdf",
             "document_other.pdf"
         ]
-        links = self.scraper.extract_all_pdf_links()
+        links = self.scraper.extract_all_links_by_extension("pdf")
         self.assertEqual(links, expected_links)
 
-    def test_extract_pdf_links_by_names(self):
+    def test_extract_links_by_names(self):
         expected_links = [
             "document_anexo_i.pdf",
             "document_anexo_ii.pdf"
         ]
-        # Usa os termos "anexo i" e "anexo ii"
-        links = self.scraper.extract_pdf_links_by_names(["anexo_i", "anexo_ii"])
+        links = self.scraper.extract_links_by_names(["anexo_i", "anexo_ii"], "pdf")
         self.assertEqual(links, expected_links)
 
 if __name__ == '__main__':
