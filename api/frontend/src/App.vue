3586c76f0b66a6203c3e12338a8d3b821b546c70
guilhermearaujom <guilherme.matos@ccc.ufcg.edu.br>
2025-04-02 21:54:30 -0300

    feat(api/frontend): implementar interface simples para busca de operadoras

---
+++ api/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SearchOperadoras />
+  </div>
+</template>
+
+<script>
+import SearchOperadoras from './components/SearchOperadoras.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchOperadoras
+  }
+}
+</script>
+
+<style>
+/* Estilos globais podem ser adicionados aqui */
+</style>
